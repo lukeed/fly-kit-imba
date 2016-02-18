ba4045d728434c0e05501c69638bed1646202ea7
luke <luke.edwards05@gmail.com>
2016-02-17 22:58:35 -0800

    add preceding semicolon for safety

--- app/scripts/sw.js
+++ app/scripts/sw.js
@@ -1,4 +1,4 @@
-(function () {
+;(function () {
 	// Check to make sure service workers are supported in the current browser,
 	// and that the current page is accessed from a secure origin.
 	// Using a service worker from an insecure origin will trigger JS console errors
